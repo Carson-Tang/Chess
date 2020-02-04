07b5ba5df13d0365657a7548c97b37a82e7dfdc0
Carson-Tang <carsonkstang@gmail.com>
2020-02-04 01:40:26 -0500

    Add constructor for Piece and Spot

--- main.cpp
+++ main.cpp
@@ -5,12 +5,50 @@
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include <QPushButton>
+#include <QLabel>
 
+class Piece {
+    QLabel *p;
+    private:
+        bool killed = false;
+        bool white  = false;
+
+    Piece(bool white){
+        this->white = white;
+    }
+
+    bool isKilled(){ return this->killed;}
+    bool isWhite(){ return this->white;}
+    bool setColor(bool white){ this->white = white;}
+    bool setKilled(bool killed){ this->killed = killed;}
+    void setPixmap(){ this->p->setPixmap(QPixmap());}
+};
+
+class Spot {
+    Piece piece;
+    private:
+        int x;
+        int y;
+
+    Spot(int x, int y, Piece piece){
+        this->setPiece(piece);
+        this->setX(x);
+        this->setY(y);
+    }
+
+    Piece getPiece(){ return this->piece;}
+    int getX(){ return this->x;}
+    int getY(){ return this->y;}
+    void setX(int x){ this->x = x;}
+    void setY(int y){ this->y = y;}
+    void setPiece(Piece piece){ this->piece = piece;}
+};
 
 void drawBoard(QGraphicsScene* scene){
     for(int i = 0; i < 8; i++){
         for(int j = 0; j < 8; j++){
             QGraphicsRectItem* grid = new QGraphicsRectItem();
+
             grid->setRect(j*100,i*100,100,100);
             if ((i+j)%2){
                 grid->setBrush(Qt::black);
@@ -20,6 +58,10 @@ void drawBoard(QGraphicsScene* scene){
             scene->addItem(grid);
         }
     }
+    QLabel* piece = new QLabel();
+    piece->setPixmap(QPixmap("C:/Users/Carson/Documents/Chess/pieces/bking.svg.png"));
+    piece->move(600,600);
+    scene->addWidget(piece);
 }
 
 int main(int argc, char *argv[])
@@ -28,12 +70,16 @@ int main(int argc, char *argv[])
     //MainWindow w;
     //w.show();
     QGraphicsScene* scene = new QGraphicsScene;
+
     drawBoard(scene);
+
     QPushButton* GenMovesButton = new QPushButton();
     GenMovesButton->setText("Generate Moves");
     GenMovesButton->move(800,0);
     scene->addWidget(GenMovesButton);
+
     QGraphicsView view(scene);
     view.show();
+
     return a.exec();
 }
