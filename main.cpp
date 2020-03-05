dac6f190c2e27b244b7e589fec1b080038952c98
Carson-Tang <carsonkstang@gmail.com>
2020-03-05 02:24:48 -0500

    Add constructor for board and piece types

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "mainwindow.h"
-
+#include <iostream>
 #include <QApplication>
 #include <QGraphicsItem>
 #include <QGraphicsScene>
@@ -10,19 +10,77 @@
 class Piece {
     QLabel *p;
     private:
-        bool killed = false;
-        bool white  = false;
+        bool killed;
+        bool white;
 
-    Piece(bool white){
-        this->white = white;
-    }
+    public:
+        Piece(bool white_ = false, bool killed_ = false) {
+            this->setColor(white_);
+            this->setKilled(killed_);
+        }
 
     bool isKilled(){ return this->killed;}
     bool isWhite(){ return this->white;}
-    bool setColor(bool white){ this->white = white;}
-    bool setKilled(bool killed){ this->killed = killed;}
+    void setColor(bool white_){ this->white = white_;}
+    void setKilled(bool killed_){ this->killed = killed_;}
     void setPixmap(){ this->p->setPixmap(QPixmap());}
 };
+
+
+class King: public Piece {
+    private:
+        bool alreadyCastled;
+
+    public:
+        King(bool white, bool alreadyCastled_ = false) {
+            this->setColor(white);
+            this->setCastled(alreadyCastled_);
+        }
+
+    bool hasCastled(){ return this->alreadyCastled;}
+    void setCastled(bool alreadyCastled_){ this->alreadyCastled = alreadyCastled_;}
+};
+
+
+class Queen: public Piece {
+    public:
+        Queen(bool white) {
+            this->setColor(white);
+        }
+};
+
+
+class Rook: public Piece {
+    public:
+        Rook(bool white) {
+            this->setColor(white);
+        }
+};
+
+
+class Bishop: public Piece {
+    public:
+        Bishop(bool white) {
+            this->setColor(white);
+        }
+};
+
+
+class Knight: public Piece {
+    public:
+        Knight(bool white) {
+            this->setColor(white);
+        }
+};
+
+
+class Pawn: public Piece {
+    public:
+        Pawn(bool white) {
+            this->setColor(white);
+        }
+};
+
 
 class Spot {
     Piece piece;
@@ -30,18 +88,64 @@ class Spot {
         int x;
         int y;
 
-    Spot(int x, int y, Piece piece){
-        this->setPiece(piece);
-        this->setX(x);
-        this->setY(y);
-    }
+    public:
+        Spot(int x_, int y_, Piece piece_): piece(piece_) {
+            this->setPiece(piece_);
+            this->setX(x_);
+            this->setY(y_);
+        }
 
     Piece getPiece(){ return this->piece;}
     int getX(){ return this->x;}
     int getY(){ return this->y;}
-    void setX(int x){ this->x = x;}
-    void setY(int y){ this->y = y;}
-    void setPiece(Piece piece){ this->piece = piece;}
+    void setX(int x_){ this->x = x_;}
+    void setY(int y_){ this->y = y_;}
+    void setPiece(Piece piece_){ this->piece = piece_;}
+};
+
+class Board {
+    private:
+        static Spot board[8][8];
+
+    public:
+        Board() {
+            this->resetBoard();
+        }
+    void resetBoard(){
+        // Black Rooks
+        this->board[0][0] = Spot(0, 0, Rook(false));
+        this->board[0][7] = Spot(0, 7, Rook(false));
+        // White Rooks
+        this->board[7][0] = Spot(7, 0, Rook(true));
+        this->board[7][7] = Spot(7, 7, Rook(true));
+        // Black Knights
+        this->board[0][1] = Spot(0, 1, Knight(false));
+        this->board[0][6] = Spot(0, 6, Knight(false));
+        // White Knights
+        this->board[7][1] = Spot(7, 1, Knight(true));
+        this->board[7][6] = Spot(7, 6, Knight(true));
+        // Black Bishops
+        this->board[0][2] = Spot(0, 2, Bishop(false));
+        this->board[0][5] = Spot(0, 5, Bishop(false));
+        // White Bishops
+        this->board[7][2] = Spot(7, 2, Bishop(true));
+        this->board[7][5] = Spot(7, 5, Bishop(true));
+        // Black Queen
+        this->board[0][3] = Spot(0, 3, Queen(false));
+        // White Queen
+        this->board[7][3] = Spot(7, 3, Queen(false));
+        // Black King
+        this->board[0][4] = Spot(0, 4, King(false));
+        // White King
+        this->board[7][4] = Spot(7, 4, King(true));
+        for(int i = 0; i < 8; i++){
+            // Black Pawns
+            this->board[1][i] = Spot(1, i, Pawn(false));
+            // White Pawns
+            this->board[6][i] = Spot(6, i, Pawn(false));
+        }
+    }
+    Spot getSpot(int x, int y){ return this->board[x][y];}
 };
 
 void drawBoard(QGraphicsScene* scene){
@@ -64,8 +168,8 @@ void drawBoard(QGraphicsScene* scene){
     scene->addWidget(piece);
 }
 
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]){
     QApplication a(argc, argv);
     //MainWindow w;
     //w.show();
